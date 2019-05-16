bb6d2d3cc02714e3dabd548c8217aa6f9deb93db
ZaxisFr <nilskrattinger@yahoo.fr>
2019-05-16 13:02:49 +0200

    feature-movement: change acronyme to lower case https://gitlab.com/FeelZoR/ile-interdite/merge_requests/2#note_170516110

--- src/ileinterdite/test/TestEtatCase.java
+++ src/ileinterdite/test/TestEtatCase.java
@@ -17,8 +17,8 @@ public class TestEtatCase {
         State[] cellState;
 
         board = new Cell[6][6];
-        DemoBoardGenarator DBG = new DemoBoardGenarator();
-        cellState = DBG.boardBuilder("../../Case.txt");
+        DemoBoardGenarator dbg = new DemoBoardGenarator();
+        cellState = dbg.boardBuilder("../../Case.txt");
         for (int j = 0; j < 6; j++) {
             for (int i = 0; i < 6; i++) {
 
