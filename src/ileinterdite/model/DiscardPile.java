ee4d2accbc4edde83b3f626f4f63af1edcd9e3de
ZaxisFr <nilskrattinger@yahoo.fr>
2019-06-06 14:33:56 +0200

    feature-flood-card : add discradPile

--- src/ileinterdite/model/DiscardPile.java
+++ src/ileinterdite/model/DiscardPile.java
@@ -1,9 +1,24 @@
 package ileinterdite.model;
 
+import ileinterdite.util.Utils;
+
 import java.util.*;
 
 public class DiscardPile {
 
-	Collection<Card> cards;
+	ArrayList<Card> cards;
+	Utils.CardType cardType;
 
+	public DiscardPile(ArrayList<Card> cards, Utils.CardType cardType) {
+		this.cards = cards;
+		this.cardType = cardType;
+	}
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	public void clearPile(){
+		this.cards = new ArrayList<>();
+	}
 }
