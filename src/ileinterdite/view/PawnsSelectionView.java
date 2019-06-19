da2b540161f901ea54464cf9ba446c29d59f7da1
ZaxisFr <nilskrattinger@yahoo.fr>
2019-06-19 17:07:42 +0200

    feature-ihm-adventurer-choice : add a cancel button

--- src/ileinterdite/view/PawnsSelectionView.java
+++ src/ileinterdite/view/PawnsSelectionView.java
@@ -8,6 +8,8 @@ import ileinterdite.util.Utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -31,6 +33,16 @@ public class PawnsSelectionView  implements IObservable<Message> {
         mainPanel = new JPanel(new BorderLayout());
 
         annulerButton = new JButton("Annuler");
+        annulerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Message m = new Message(Utils.Action.CANCEL_ACTION, null);
+                pawnsIco.clear();
+                choicePanel = null;
+                window.setVisible(false);
+                notifyObservers(m);
+            }
+        });
         choicePanel = new JPanel();
 
 
