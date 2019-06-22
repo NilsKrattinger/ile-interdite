eabfbbcc5481bb58077b13b76d1f9cfc3b65ce55
Feelzor <losdingtro@gmail.com>
2019-06-22 12:36:16 +0200

    feature-repair-swing: Use InvokeLater in VictoryView

--- src/ileinterdite/view/VictoryView.java
+++ src/ileinterdite/view/VictoryView.java
@@ -8,19 +8,21 @@ public class VictoryView {
     private JPanel mainPanel;
 
     public VictoryView() {
-        mainPanel = new JPanel(new BorderLayout());
+        SwingUtilities.invokeLater(() -> {
+            mainPanel = new JPanel(new BorderLayout());
 
-        JLabel label = new JLabel("Félicitations, vous avez gagné !");
-        label.setFont(new Font(label.getFont().getName(), label.getFont().getStyle(), 24));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
+            JLabel label = new JLabel("Félicitations, vous avez gagné !");
+            label.setFont(new Font(label.getFont().getName(), label.getFont().getStyle(), 24));
+            label.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JLabel deathLabel = new JLabel("Vous avez su déjouer l'Ile Interdite et lui subtiliser ses précieux trésors !");
-        deathLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        deathLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+            JLabel deathLabel = new JLabel("Vous avez su déjouer l'Ile Interdite et lui subtiliser ses précieux trésors !");
+            deathLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            deathLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
 
-        mainPanel.add(label, BorderLayout.CENTER);
-        mainPanel.add(deathLabel, BorderLayout.SOUTH);
-        mainPanel.revalidate();
+            mainPanel.add(label, BorderLayout.CENTER);
+            mainPanel.add(deathLabel, BorderLayout.SOUTH);
+            mainPanel.revalidate();
+        });
     }
 
     public JPanel getMainPanel() {
